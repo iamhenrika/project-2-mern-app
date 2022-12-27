82b7dff1247a305c70d06b63e59bee4cd17103aa
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-12-26 22:02:15 -0500

    Update

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,20 +1,19 @@
-const User = require('../../models/user')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   create,
   login,
-  checkToken
-}
-
+  checkToken,
+};
 
 async function create(req, res) {
   try {
     // Add the user to the database
-    const user = await User.create(req.body)
+    const user = await User.create(req.body);
 
-    const token = createJWT(user)
+    const token = createJWT(user);
     // Yes, we can use res.json to send back just a string
     // The client code needs to take this into consideration
     res.json(token);
@@ -27,34 +26,31 @@ async function create(req, res) {
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({ email: req.body.email})
-    if(!user) throw new Error()
-    const match = await bcrypt.compare(req.body.password, user.password)
-    if (!match) throw new Error()
-    res.json(createJWT(user))
-  } catch(err) {
-     // Client will check for non-2xx status code
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    res.json(createJWT(user));
+  } catch (err) {
+    // Client will check for non-2xx status code
     // 400 = Bad Request
     res.status(400).json(err);
   }
 }
 
-
 function checkToken(req, res) {
   // req.user will always be there for you when a token is sent
-  console.log('req.user', req.user);
+  console.log("req.user", req.user);
   res.json(req.exp);
 }
-
-
 
 /*-- Helper Functions --*/
 
 function createJWT(user) {
   return jwt.sign(
     // data payload
-    {user},
+    { user },
     process.env.SECRET,
-    {expiresIn: "24h"}
-  )
+    { expiresIn: "24h" }
+  );
 }
