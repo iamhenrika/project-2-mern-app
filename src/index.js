ecf2391bbb50215d3606066d431a3ba1c0350df8
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2023-01-01 22:45:08 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CreatePost from "./CreatePost";
+import Posts from "./Posts";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/create" element={<CreatePost />}></Route>
+        <Route path="/create/posts" element={<Posts />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
